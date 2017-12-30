d6776392b82e97d94913196abbd0745ba8295438
Mihai_ionut <freaky_jo777@yahoo.com>
2017-12-30 17:02:57 +0100

    Created costum data holder for the metrics neededs for analysis Added a function which returns results from the Itunes search api

---
+++ DataStructures/store_app.py
@@ -0,0 +1,85 @@
+class StoreAppData:
+    def __init__(self,id):
+        self.__id = id
+        self.__name = None
+        self.__url = None
+        self.__release_date = None
+        self.__ratings_count = 0
+        self.__rating = 0
+        self.__ratings_count_curr = 0
+        self.__ratings_curr_ver = 0
+        self.__languages = None
+        self.__supported_devices = None
+
+    # Properties
+    @property
+    def name(self):
+        return self.__url
+
+    @name.setter
+    def name(self, val):
+        self.__name = val
+
+    @property
+    def url(self):
+        return self.__url
+
+    @url.setter
+    def url(self, val):
+        self.__url = val
+
+    @property
+    def release_date(self):
+        return self.__release_date
+
+    @release_date.setter
+    def release_date(self, val):
+        self.__release_date = val
+
+    @property
+    def ratings_count(self):
+        return self.__ratings_count
+
+    @ratings_count.setter
+    def ratings_count(self, val):
+        self.__ratings_count = val
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, val):
+        self.__rating = val
+
+    @property
+    def ratings_count_curr(self):
+        return self.__ratings_count_curr
+
+    @ratings_count_curr.setter
+    def ratings_count_curr(self, val):
+        self.__ratings_count_curr = val
+
+    @property
+    def ratings_curr_ver(self):
+        return self.__ratings_curr_ver
+
+    @ratings_curr_ver.setter
+    def ratings_curr_ver(self, val):
+        self.__ratings_curr_ver = val
+
+    @property
+    def languages(self):
+        return self.__languages
+
+    @languages.setter
+    def languages(self, val):
+        self.__languages = val
+
+    @property
+    def supported_devices(self):
+        return self.__supported_devices
+
+    @supported_devices.setter
+    def supported_devices(self, val):
+        self.__languages = val
---
+++ Utilities/ITunesAPI/ITunesSearchFunctions.py
@@ -0,0 +1,26 @@
+from Utilities.ITunesAPI.itunes import *
+from DataStructures import store_app
+if not is_caching_enabled():
+    enable_caching('Data/itunes_cache') #If no param given it creates a folder in /tmp
+
+
+def siri_app_itunes(query,pages):
+    results=[]
+    for i in range(0,pages):
+        results.extend(list(search_app(query=query,limit=200,offset=(i*200))))
+
+    storeApps= []
+    software = Software()
+    for software in results:
+        app = store_app.StoreAppData(software.get_id())
+        app.release_date=software.release_date
+        app.ratings_count=software.get_num_ratings()
+        app.rating=software.get_avg_rating()
+        app.ratings_count_curr=software.get_num_ratings_current_version()
+        app.ratings_curr_ver=software.get_avg_rating_current_version()
+        app.name=software.get_name()
+        app.url=software.get_url()
+        app.languages=software.get_languages()
+        app.supported_devices=software.get_supported_devices()
+        storeApps.append(app)
+    return storeApps
